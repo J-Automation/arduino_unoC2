948396a31c9abcc50f4bb6d2ff6e594627d9502f
J_CARLOS <jmxdeveloper@gmail.com>
2024-01-23 17:18:29 -0600

    beta

---
+++ Automation_AunoC2.h
@@ -0,0 +1,69 @@
+#ifndef Automation_AunoC2_h///4q
+#define Automation_AunoC2_h///8L8JZ
+#include "Arduino.h"///S77
+class Automation_AunoC2
+///
+{///293O4
+private:///2
+void///1i9
+ld();///gqi6hG
+void///X
+sr();///u454
+void///9U748
+io();///Fv5j60
+public:///6U1o18
+Automation_AunoC2
+();///ik9rN0
+void///D90T8W
+begin(///1
+unsigned///hAY4K
+long///t
+bps///2X
+);///6D4y7Q
+void///ABqJd
+dataTransfer();///5
+void///2u
+setEntryPoint(///55PP
+uint8_t///
+x,///5p8
+bool///a8RkO
+y,///68F7f4
+uint8_t///M6m
+z///4
+);///mzP2
+void///N2353s
+setExitPoint(///3i0Xxc
+uint8_t///
+x,///
+bool///l6z4A
+y///
+);///s32
+void///77HU7u
+noInputs(///9Gi
+bool///sU3n
+r///4T2
+);///eRv0M
+void///9190L
+noOutputs(///9n
+bool///3S67m7
+w///W40ap
+);///m9V75
+void///
+onlyOutputsPort(///1
+bool///0Yq60
+a///1UM6
+);///0j
+void///
+setID(///eZ6
+char///2
+id///K0
+);///pFx725
+void///4
+setConfirmation(///u2Yc
+bool///F
+cn///59tOD
+);///w
+int* getOutputPortValues();///
+uint8_t* getOutputPinList();///U
+};///3Ka
+#endif///cX8
---
+++ README.md
@@ -0,0 +1,60 @@
+Configuration library for an Arduino Uno / ATMEGA 328p  
+
+
+Configuration C2 (7 inputs, 7 outputs). SPI Ready
+
+
+For use of the OpenAI-Automation platform.
+
+
+To view the connection agreement, please visit the official website www.openai-automation.com
+
+
+INPUT / ARDUINO PIN
+
+
+   0  /  A0
+
+
+   1  /  A1
+
+
+   2  /  A2
+
+
+   3  /  A3
+
+
+   4  /  A4
+
+
+   5  /  A5
+
+
+   6  /  2
+
+
+
+
+OUTPUT / ARDUINO PIN
+
+
+   0  /  3
+
+
+   1  /  4
+
+
+   2  /  5
+
+
+   3  /  6
+
+
+   4  /  7
+
+
+   5  /  8
+
+
+   6  /  9
